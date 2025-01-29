d05db925da6297cf415ec055487a535bd22eca24
Qingyu255 <qingyu2555@gmail.com>
2025-01-30 03:12:26 +0800

    IS4-11: Set up nextjs with shadcn

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { ThemeModeToggle } from "@/components/ThemeModeToggle";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <ThemeModeToggle/>
+        Hello Photo Editor
+      </div>
+    </>
+  );
+}
