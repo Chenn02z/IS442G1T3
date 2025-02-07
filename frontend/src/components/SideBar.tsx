060c04f120813343dfa51dfc1bc3416484527652
Yu Chen <yeoyuchen@gmail.com>
2025-02-07 08:55:40 +0800

    sidebar menu for crop function

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { Crop } from "lucide-react";
 import { SquareX } from "lucide-react";
 import { ThemeModeToggle } from "@/components/ThemeModeToggle";
+import CropSidebar from "@/components/CropSideBar";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 const SideBar = () => {
   return (
     <div className="fixed flex flex-col space-y-2 w-fit h-full p-2 items-center border-2 border-r-lightgray-500">
       <ThemeModeToggle />
       <p className="text-xs pt-3 pb-1 font-semibold">Edit:</p>
-      <div className="border border-1 p-1 rounded-md cursor-pointer">
-        <Crop />
-      </div>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <div className="border border-1 p-1 rounded-md cursor-pointer">
+              <CropSidebar />
+            </div>
+          </TooltipTrigger>
+          <TooltipContent side="right">
+            <p>Crop Image</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
       <div className="border border-1 p-1 rounded-md cursor-pointer">
         <SquareX />
       </div>
