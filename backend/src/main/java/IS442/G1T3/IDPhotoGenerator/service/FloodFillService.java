ed062f52d45530937303466f9a6b60aa9ea5bed7
Lim Jitt Hing <jhlim.2023@smu.edu.sg>
2025-02-17 21:49:45 +0800

    frontend CHANGES YAY IT WORKS (clicking functionality)

--- backend/src/main/java/IS442/G1T3/IDPhotoGenerator/service/FloodFillService.java
+++ backend/src/main/java/IS442/G1T3/IDPhotoGenerator/service/FloodFillService.java
@@ -5,5 +5,14 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 public interface FloodFillService {
+    /**
+     * Processes the crop request for an image.
+     *
+     * @param file       The multipart file of the image to process.
+     * @param seedX      The x-coordinate of the pixel chosen.
+     * @param seedY      The y-coordinate of the pixel chosen.
+     * @param tolerance  The tolerance of colour difference between surrounding pixels.
+     * @return           The processed image with pixels in a byte array.
+     */
     byte[] removeBackground(MultipartFile file, int seedX, int seedY, int tolerance) throws IOException;
 }
