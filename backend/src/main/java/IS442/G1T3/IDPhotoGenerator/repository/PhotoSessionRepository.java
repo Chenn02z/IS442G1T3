8531ce7f7408677c5a0629ca1c48bf385ba77c62
eeherngtan <work.eeherng@gmail.com>
2025-04-08 17:12:00 +0800

    fix crop refactoring issue

--- backend/src/main/java/IS442/G1T3/IDPhotoGenerator/repository/PhotoSessionRepository.java
+++ backend/src/main/java/IS442/G1T3/IDPhotoGenerator/repository/PhotoSessionRepository.java
@@ -1,15 +1,9 @@
 package IS442.G1T3.IDPhotoGenerator.repository;
-import java.awt.*;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import IS442.G1T3.IDPhotoGenerator.model.ImageNewEntity;
 import IS442.G1T3.IDPhotoGenerator.model.PhotoSession;
 
 @Repository
